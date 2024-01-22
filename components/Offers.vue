91e81aa61e1e01c7be5d275a797ec847def5b1b5
Magomed Isaev <critikk99@bk.ru>
2024-01-22 20:13:55 +0300

    feat: add animations

--- components/Offers.vue
+++ components/Offers.vue
@@ -1,6 +1,6 @@
 <script setup lang="tsx">
 const pictures = ['offer1', 'offer2', 'offer3'] 
-const PictureComponent = (props: {picture: string}) => <div class="w-full overflow-hidden relative md:h-40 lg:h-52  md:w-96">
+const PictureComponent = (props: {picture: string}) => <div class="w-full overflow-hidden relative md:h-40 lg:h-52  md:w-96 lg:w-[472px]">
 	<div class="text-white translate-x-[-50%] translate-y-[-50%] left-1/2 top-1/2 absolute z-[1] text-center">
 		<h3 class="md:text-lg lg:text-2xl text-2xl text-bold top-3">
 			Свежие цветы
@@ -9,7 +9,15 @@ const PictureComponent = (props: {picture: string}) => <div class="w-full overfl
 			купить
 		</button>
 	</div>
-	<img src={`./${props.picture}.jpeg`} class="object-cover transition ease-linear duration-1000 brightness-50 delay-0 hover:scale-110 h-full w-full"/>
+	<img src={`./${props.picture}.jpeg`} class="object-cover
+		transition
+		ease-linear
+		duration-1000
+		brightness-50
+		delay-0
+		hover:scale-110
+		h-full
+		w-full"/>
 </div>
 </script>
 
